f2d4af7f4e8b443f070bf2d72baf72001f469d74
zhouyuan <3623936813@qq.com>
2020-09-09 17:40:56 +0800

    priority_queue_protice*

--- protice/10.priority_queue_protice.cpp1.c
+++ protice/10.priority_queue_protice.cpp1.c
@@ -1,26 +1,30 @@
 /*************************************************************************
-	> File Name: 10.priority_queue.cpp
-	> Author: 
-	> Mail: 
-	> Created Time: Sat Jul  4 15:24:21 2020
+	> File Name: 10.priority_queue_protice.cpp1.c
+	> Author: zhouyuan
+	> Mail: 3294207721@qq.com 
+	> Created Time: 2020年09月09日 星期三 14时04分19秒
  ************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
 #include<time.h>
+#include<unistd.h>
+ 
 #define swap(a, b) {\
-    __typeof(a) __temp = (a);\
-    (a) = (b), (b) = __temp;\
+    __typeof(a) __tmp = (a);\
+    (a) = (b), (b) = __tmp;\
 }
 
 typedef struct priority_queue {
     int *data;
-    int cnt, size;
+    int size, cnt;
 } priority_queue;
 
 priority_queue *init(int n) {
     priority_queue *q = (priority_queue *)malloc(sizeof(priority_queue));
-    q->data = (int *)malloc(sizeof(int) * (n  + 1));
+    q->data = (int *)malloc(sizeof(int) * (n + 1));
     q->cnt = 0;
     q->size = n;
     return q;
@@ -30,7 +34,7 @@ int empty(priority_queue *q) {
     return q->cnt == 0;
 }
 
-int top(priority_queue *q) {
+int top(priority_queue * q) {
     return q->data[1];
 }
 
@@ -39,18 +43,19 @@ int push(priority_queue *q, int val) {
     if (q->cnt >= q->size) return 0;
     q->data[++(q->cnt)] = val;
     int ind = q->cnt;
-    while (ind >> 1 && q->data[ind] > q->data[ind >> 1]) {
+    while(ind >> 1 && q->data[ind] > q->data[ind >> 1]) {
         swap(q->data[ind], q->data[ind >> 1]);
         ind >>= 1;
     }
     return 1;
 }
 
-void update(priority_queue *q, int ind) {
-    while ((ind << 1) <= q->cnt) {
+
+void updata(priority_queue *q,  int ind) {
+    while((ind << 1) <= q->cnt) {
         int max = ind, l = ind << 1, r = ind << 1 | 1;
         if (l < q->cnt && q->data[l] > q->data[max]) max = l;
-        if (l < q->cnt && q->data[r] > q->data[max]) max = r;
+        if (r < q->cnt && q->data[r] > q->data[max]) max = r;
         if (max == ind) break;
         swap(q->data[ind], q->data[max]);
         ind = max;
@@ -62,7 +67,7 @@ int pop(priority_queue *q) {
     if (q == NULL) return 0;
     if (empty(q)) return 0;
     q->data[1] = q->data[q->cnt--];
-    update(q, 1);
+    updata(q, 1);
     return 1;
 }
 
@@ -81,16 +86,22 @@ void clear(priority_queue *q) {
     return ;
 }
 
-int main () {
-    srand (time(0));
+
+int main() {
+    srand(time(0));
     #define max_op 20
     priority_queue *q = init(max_op);
-    for (int i = 0; i < max_op; i++) {
+    for (int i = 0;i < max_op; i++) {
         int val = rand() % 100;
         push(q, val);
-        printf("insert %d to priority_queue\n", val);
+        printf("insert %d to priority_queue!\n", val);
     }
-    output(q);
+
+    for (int i = 0; i < max_op; i++) {
+        printf("%d ", top(q));
+        pop(q);
+    }
+    printf("\n");
     clear(q);
     return 0;
 }
---
+++ protice/10.priority_queue_protice.cpp1.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 10.priority_queue_protice.cpp1.cpp
+	> Author: zhouyuan
+	> Mail: 3294207721@qq.com 
+	> Created Time: 2020年09月09日 星期三 14时04分04秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+ 
+int main() {
+ 
+ 
+    return 0;
+}
